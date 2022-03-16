7e0b98949d54198cf2ef48d8254e66845205b3ce
agus4385 <101607137+agus4385@users.noreply.github.com>
2022-03-16 13:02:01 +0700

    Update

--- kalkulator.cs
+++ kalkulator.cs
@@ -1,6 +1,20 @@
 class Calculator
 {
 	static void main (string []agrs){
+		int a = 20;
+		int b = 2; //Tugas 4
+
+			Console.WriteLine("Hasil pengurangan {0} + {1} = {2}",a ,b, Pengurangan(a,b));
+            Console.WriteLine("Hasil pengurangan {0} - {1} = {2}",a ,b, Pengurangan(a,b));
+
+            Console.WriteLine("\n Tekan sembarang tombol untuk keluar");
+            Console.ReadKey();
+	}
+	static int Penambahan(int a, int b){
+		return a + b; //Tugas 2
+	}
+	static int Pengurangan(int a, int b){
+		return a - b; //Tugas 3
 		
 	}
 }
