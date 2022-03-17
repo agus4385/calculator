5e234b67f9b3c6ab7fd77c12d8dc1ada88d6f579
agus4385 <101607137+agus4385@users.noreply.github.com>
2022-03-17 10:44:04 +0700

    Upodate tugas

--- kalkulator.cs
+++ kalkulator.cs
@@ -1,20 +1,43 @@
-class Calculator
-{
-	static void main (string []agrs){
-		int a = 20;
-		int b = 2; //Tugas 4
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-	    Console.WriteLine("Hasil pengurangan {0} + {1} = {2}",a ,b, Pengurangan(a,b));
+namespace CalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Apikasi Kalkulator";
+
+            int a = 20;
+            int b = 2;
+
+            Console.WriteLine("Hasil penambahan" + a +" + "+ b +" = "+ Penambahan(a,b));
             Console.WriteLine("Hasil pengurangan {0} - {1} = {2}",a ,b, Pengurangan(a,b));
+            Console.WriteLine("Hasil perkalian {0} * {1} = {2}",a ,b, Perkalian(a,b));
+            Console.WriteLine("Hasil pembagian {0} / {1} = {2}",a ,b, Pembagian(a,b));
 
             Console.WriteLine("\n Tekan sembarang tombol untuk keluar");
             Console.ReadKey();
-	}
-	static int Penambahan(int a, int b){
-		return a + b; //Tugas 2
-	}
-	static int Pengurangan(int a, int b){
-		return a - b; //Tugas 3
-		
-	}
+        }
+        static int Penambahan(int a,int b)
+        {
+            return a + b;
+        }
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int Perkalian( int a, int b)
+        {
+            return a * b;
+        }
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        } 
+    }
 }
